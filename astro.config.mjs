fb264ddb2c981efbe2dbae4dcfd32942c83f2b08
Mohammed Ihsan <ihsanvp2738@gmail.com>
2022-10-24 19:19:52 +0530

    add compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://awcqatarlogistics.com",
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), compress()]
 });
