49c6f4bf19aadda4f173da38434c54cfb2440558
ihsanvp <ihsanvp2738@gmail.com>
2022-10-24 20:40:32 +0530

    add basic header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#9C2C4F"
+      }
+    },
   },
   plugins: [],
 };
